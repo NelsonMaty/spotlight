913f87d0b6a8656c4a87769b29f4179d37eaa0b4
Nelson M. Rios <nelson.rios@unc.edu.ar>
2023-02-19 17:33:28 -0300

    Create nav list component

---
+++ src/components/NavList.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <ul class="flex rounded-full px-3 bg-white/90 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 ">
+      <li> <a href="#" class="px-3 py-2 block hover:text-teal-500 dark:hover:text-teal-400 transition">About</a> </li>
+      <li> <a href="#" class="px-3 py-2 block hover:text-teal-500 dark:hover:text-teal-400 transition">Articles</a> </li>
+      <li> <a href="#" class="px-3 py-2 block hover:text-teal-500 dark:hover:text-teal-400 transition">Projects</a> </li>
+      <li> <a href="#" class="px-3 py-2 block hover:text-teal-500 dark:hover:text-teal-400 transition">Speaking</a> </li>
+      <li> <a href="#" class="px-3 py-2 block hover:text-teal-500 dark:hover:text-teal-400 transition">Uses</a> </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <div class="pt-6 w-full pointer-events-auto">
+      <div class="mx-auto max-w-7xl lg:px-8">
+        <div class="mx-auto max-w-2xl lg:max-w-5xl duration-1000 transition-all ">
+          <div class="flex gap-4">
+            <div class="flex flex-1"></div>
+            <div class="flex flex-1">
+              <NavList />
+            </div>
+            <div class="flex flex-1 justify-end -mt-1">
+              <DarkModeButton />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup>
+import DarkModeButton from "./DarkModeButton.vue";
+import NavList from "./NavList.vue";
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
