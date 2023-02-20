d118c6869a0e519593f2e48f35976e2b56da10e6
Nelson M. Rios <nelson.rios@unc.edu.ar>
2023-02-20 18:45:03 -0300

    Add main landing intro content

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import Background from "./components/Background.vue";
 import PageContainer from "./components/PageContainer.vue";
 import PageHeader from "./components/PageHeader.vue";
+import LandingMain from "./components/LandingMain.vue";
 </script>
 
 <template>
   <Background>
     <PageContainer>
       <PageHeader />
+      <LandingMain />
     </PageContainer>
   </Background>
 </template>
