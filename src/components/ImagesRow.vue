54e9db416a788f10a50abea41f3cc41bd2178060
Nelson M. Rios <nelson.rios@unc.edu.ar>
2023-02-25 17:24:13 -0300

    Add rof of images to main page

---
+++ src/components/ImagesRow.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex justify-center gap-5">
+    <RoundedImage class="rotate-2" source="src/assets/image-1.webp" />
+    <RoundedImage class="-rotate-2" source="src/assets/image-2.webp" />
+    <RoundedImage class="rotate-2" source="src/assets/image-3.webp" />
+    <RoundedImage class="rotate-2" source="src/assets/image-4.webp" />
+    <RoundedImage class="-rotate-2" source="src/assets/image-5.webp" />
+  </div>
+</template>
+
+<script setup>
+import RoundedImage from "./RoundedImage.vue";
+</script>
---
+++ src/components/RoundedImage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="aspect-[9/10] w-72 flex-none overflow-hidden rounded-xl object-cover"
+  >
+    <img :src="source" alt="" class="h-full w-full object-cover" />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  source: String,
+});
+</script>
